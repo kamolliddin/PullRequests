a6de5059b980ad5a5d06f2f1f709432ce59e4d4f
Kamoliddin <nabijonovkamol@gmail.com>
2020-09-12 20:21:18 +0500

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,8 @@
+# Python program to perform +, -, * and / of two integer numbers:
+x1 = int(input("Enter First Number: "))
+x2 = int(input("Enter Second Number: "))
+
+print("x1 + x2: ", x1 + x2)
+print("x1 - x2: ", x1 - x2)
+print("x1 * x2: ", x1 * x2)
+print("x1 / x2: ", x1 / x2)
